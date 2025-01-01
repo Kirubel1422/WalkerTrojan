0ab955baa8fd77b53e43ec5a43fd205ebe928fb5
kirubel <kirubel739@gmail.com>
2025-01-02 00:59:28 +0400

    updates walker.py

--- modules/walker.py
+++ modules/walker.py
@@ -2,7 +2,7 @@ import os
 
 def run(**args):
     files_list = list()
-    
+    print('[+] In walker.py')
     for root, _, fname in os.walk('/'):
         files_list.append(str(root) + "/" + fname)
     
